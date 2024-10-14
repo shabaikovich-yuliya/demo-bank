7f2c171f136e06ba4b690fb6f3e76b843a8bc94b
Yuliya <yuliya.01707@gmail.com>
2024-10-14 22:23:27 +0200

    add all changes

--- src/UseAccount.java
+++ src/UseAccount.java
@@ -6,16 +6,20 @@ public class UseAccount {
     public static void main(String[] args) {
         Account myAccount = new Account();
         Account yourAccount = new Account();
+
         myAccount.name = "Барри";
         myAccount.address = "222 Inner Lane";
         myAccount.balance = 24.02;
+
         yourAccount.name = "Джейн";
         yourAccount.address = "121 Outer Street";
         yourAccount.balance = 55.63;
+
         myAccount.display();
         out.print(" плюс $");
         out.printf("$%4.2f", myAccount.getInterest(5.00));
         out.println(" дохода");
+
         yourAccount.display();
         double yourInterestRate = 7.00;
         out.print(" плюс $");
