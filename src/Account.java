c9458c62cbddfeb6984e9f9731c588c3e167e27a
Yuliya <yuliya.01707@gmail.com>
2024-10-14 22:16:13 +0200

    refactor account class

--- src/Account.java
+++ src/Account.java
@@ -6,13 +6,15 @@ public class Account {
     String name;
     String address;
     double balance;
+
     public void display() {
         out.print(name);
-        out.print("(");
+        out.print(" (");
         out.print(address);
         out.print(") имеет на счету $");
         out.print(balance);
     }
+
     public double getInterest(double percentageRate) {
         return balance * percentageRate / 100.00;
     }
