16d0f67d4312e033c85cedb2de715c62b6a1b515
Yuliya <yuliya.01707@gmail.com>
2024-10-14 21:41:54 +0200

    add account class

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+package src;
+
+import static java.lang.System.out;
+
+public class Account {
+    String name;
+    String address;
+    double balance;
+    public void display() {
+        out.print(name);
+        out.print("(");
+        out.print(address);
+        out.print(") имеет на счету $");
+        out.println(balance);
+    }
+    public double getInterest(double percentageRate) {
+        return balance * percentageRate / 100.00;
+    }
+}
